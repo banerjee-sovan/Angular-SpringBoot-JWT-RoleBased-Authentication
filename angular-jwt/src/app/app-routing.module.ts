45f5d26a3b5e87b3a4ae779c9501a3212363d477
banerjee-sovan <jgec.sovon93@gmail.com>
2023-03-16 09:41:31 +0530

    commit navbar and routing

--- angular-jwt/src/app/app-routing.module.ts
+++ angular-jwt/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
 
 const routes: Routes = [
-  {path:'',component:LoginComponent}
+  {path:'',component:LoginComponent},
+  {path:'home',component:HomeComponent},
+  {path:'user-dashboard',component:UserDashboardComponent},
+  {path:'admin-dashboard',component:AdminDashboardComponent}
 ];
 
 @NgModule({
